856c105437799c7cd280d879667061435af40725
Kirill Tkachenko <kirill.tkachenko7@gmail.com>
2020-01-26 18:27:05 +0300

    fixed db.sqlite3

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
+from .models import Post , Group
 
 
 def index(request):
